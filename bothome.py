2885a3a2ab81a05ca2d2bfaa9487a361f5fde426
mixej <mixeev.v@gmail.com>
2022-03-23 00:54:54 +0300

    comment code

--- bothome.py
+++ bothome.py
@@ -25,11 +25,14 @@ SLEEP_TIMEOUT = 300
 # класс ведения лога показаний с датчиков
 class LogWriter: 
 
+
 	# метод для создания шапки лога, проверяет наличие запесей в файле и при отсутствии таковых записывает шапку
 	def write_header(self):		
 		with open(FILENAME,'a+') as file:
 			if os.stat(FILENAME).st_size == 0:
 				file.write('Date,Time,Temp,Hum,Press\r\n')
+				
+				
 	# метод считывает показания с датчиков и пишет их в лог фаил			
 	def write_line(self):
 		h, t = dht.read_retry(dht.DHT22, DHT_PIN)
@@ -39,6 +42,8 @@ class LogWriter:
 				file.write('{0},{1},{2:0.1f},{3:0.1f},{4:0.1f},\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), t, h, p/133.3))
 		else:
 			print("Failed to retrieve data from humidity sensor")
+			
+			
 	# метод для запуска функций лога данных в фаил 
 	# проверяет наличие шапки и пишет показания с указанным интервалом			
 	def start(self):
@@ -55,7 +60,6 @@ def start_bot():
 		
 		
 @dispb.message_handler(commands=["start"])		
-#@db.message_handler()
 async def start_comand(message: types.Message):
 	await message.answer("Привет! какие показания тебя интересуют?\r\n /temp-Температура\r\n /hum-Влажность\r\n /press-Давление")
 
@@ -63,20 +67,17 @@ async def start_comand(message: types.Message):
 async def get_parametrs(message: types.Message):
 
 	h, t = dht.read_retry(dht.DHT22, DHT_PIN)
-	#p = bmp.read_pressure()
 	await message.answer("{0} Температура в комнате, {1:.1f}С".format(time.strftime('%H:%M'), t))
 	
 @dispb.message_handler(commands=["hum"])
 async def get_parametrs(message: types.Message):
 
 	h, t = dht.read_retry(dht.DHT22, DHT_PIN)
-	#p = bmp.read_pressure()
 	await message.answer("{0} Влажность в комнате, {1:.1f}%".format(time.strftime('%H:%M'), h))
 	
 @dispb.message_handler(commands=["press"])
 async def get_parametrs(message: types.Message):
 
-	#h, t = dht.read_retry(dht.DHT22, DHT_PIN)
 	p = bmp.read_pressure()
 	await message.answer("{0} Давление, {1:.1f} мм рт.ст".format(time.strftime('%H:%M'), p/133.3))	
 
