7c1ea0ed8b1a03c79b5275eead0b023db21e0362
mixej <mixeev.v@gmail.com>
2022-04-08 23:52:51 +0300

    new file

--- logwriter.py
+++ logwriter.py
@@ -31,7 +31,8 @@ class LogWriter:
 #		os.replace(FILENAME, DIRNAME)
 		os.rename(FILENAME, new_file)
 #		shutil.copy2(FILENAME, DIRNAME)
-		os.system('r' >FILENAME)
+		open(FILENAME,"w").close()
+#		os.system('r' >FILENAME)
 							
 	# метод считывает показания с датчиков и пишет их в лог фаил			
 	def write_line(self):
