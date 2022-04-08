4c76265841394cec997e164b5ec23953f74b4421
mixej <mixeev.v@gmail.com>
2022-04-08 23:23:29 +0300

    new file

--- logwriter.py
+++ logwriter.py
@@ -32,8 +32,8 @@ class LogWriter:
 #		if os.stat(FILENAME).st_size >= 150:
 #		os.replace(FILENAME, DIRNAME)
 #		os.rename(DIRNAME/FILENAME, DIRNAME/FILENAME + time.strftime('%H:%M') + '.csv')
-		shutil.copy2(FILENAME, DIRNAME/self.new_filename())
-		os.system(r >FILENAME)
+		shutil.copy2('FILENAME', 'DIRNAME/self.new_filename()')
+		os.system(r' >FILENAME')
 							
 	# метод считывает показания с датчиков и пишет их в лог фаил			
 	def write_line(self):
