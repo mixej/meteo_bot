22fc37f475ddc7b153a003967893c6c92be57a1c
mixej <mixeev.v@gmail.com>
2022-04-26 20:18:49 +0300

    new file

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ dispb = Dispatcher(bot)
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons = ["температура", "влажность", "давление"]
 buttons_2 = ["сервис"]
-keyboard.add(*buttons).add(KeyboardButton(buttons_2))
+keyboard.add(*buttons).add(buttons_2)
 #markup5 = ReplyKeyboardMarkup().row(button1, button2, button3).add(KeyboardButton('Средний ряд'))
 
 
